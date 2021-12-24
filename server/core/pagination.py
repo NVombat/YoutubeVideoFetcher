e43f93c05233febdc528da607e4e2f66213edbb6
NVombat <nvombatkere@gmail.com>
2021-12-25 00:27:21 +0530

    :memo: added comments

--- server/core/pagination.py
+++ server/core/pagination.py
@@ -4,12 +4,25 @@ from collections import OrderedDict
 
 
 class CustomPagination(pagination.PageNumberPagination):
+    """
+    Define Custom Pagination Characteristics
+    """
     page_size = 5
     page_size_query_param = "page_size"
     max_page_size = 20
     page_query_param = "page"
 
-    def get_paginated_response(self, data):
+    def get_paginated_response(self, data) -> Response:
+        """
+        Returns a paginated response with meta data related
+        to the pagination
+
+        Args:
+            data = Data to be returned
+
+        Returns:
+            Response
+        """
         next = None
         previous = None
 
